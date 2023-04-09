6cd5754f55757a96c26b94cc10baff605fea9090
Liyb2002 <yl4092@columbia.edu>
2023-04-09 17:21:29 -0400

    produce

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import write2JSON
+import intersection
+import produce
+import generic_objects
+
+import numpy as np
+
+#find impossible intersection positions
+startPos = np.array([400,400])
+basic_scene = intersection.Scene(startPos)
+foreground_index = 8
+background_index = 12
+
+foreground_intersection = basic_scene.get_possible_intersects(foreground_index)
+background_intersection = basic_scene.get_possible_intersects(background_index)
+
+#read the inputs
+object_list = []
+with open('objects.json', 'r') as object_file:
+    objects_data = json.load(object_file)
+
+    object_list.append(objects_data[0])
+    for object_data in objects_data:
+        new_object = generic_objects.Generic_object(object_data)
+        object_list.append(new_object)
+
+#start produce
+production_list = produce.execute_model(foreground_intersection, object_list)
+
+
+
+write2JSON.write(production_list)
---
+++ produce.py
@@ -0,0 +1,27 @@
+import generic_objects
+import procedural_objects
+
+def execute_model(start_pos, object_list):
+    production_list = []
+
+    cur_id = 1
+    start_scope = object_list[cur_id].scope
+    cur_obj = procedural_objects.Procedural_object(cur_id, start_pos, start_scope)
+    production_list.append(cur_obj)
+
+    #processing
+    # count = 0
+    while(True):
+        cur_generic_obj = object_list[cur_id]
+        next_id = cur_generic_obj.get_next()
+        if next_id == None:
+            break
+        next_obj = procedural_objects.Procedural_object(next_id, start_pos, start_scope)
+        choice = cur_generic_obj.execute_rule(next_id)
+        next_obj.set_position(cur_obj, choice)
+
+        cur_id = next_id
+        cur_obj = next_obj
+        production_list.append(cur_obj)
+    
+    return production_list
