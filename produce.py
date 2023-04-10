762ee4d6093e1b2244b005045890d20dcfa5e8bd
Liyb2002 <yl4092@columbia.edu>
2023-04-09 21:49:54 -0400

    works

--- produce.py
+++ produce.py
@@ -16,9 +16,12 @@ def execute_model(start_pos, object_list):
         next_id = cur_generic_obj.get_next()
         if next_id == None:
             break
-        next_obj = procedural_objects.Procedural_object(next_id, start_pos, start_scope)
-        choice = cur_generic_obj.execute_rule(next_id)
-        next_obj.set_position(cur_obj, choice)
+        
+        next_generic_obj = object_list[next_id]
+        next_scope = next_generic_obj.scope
+        next_obj = procedural_objects.Procedural_object(next_id, start_pos, next_scope)
+        next_choice = cur_generic_obj.execute_rule(next_id)
+        next_obj.set_position(cur_obj, next_choice)
 
         cur_id = next_id
         cur_obj = next_obj
