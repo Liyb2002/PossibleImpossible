d9afc45ff241c2f079d4801c5be907153f39649e
Liyb2002 <yl4092@columbia.edu>
2023-04-30 18:53:54 -0400

    clean up ++

--- particle.py
+++ particle.py
@@ -1,6 +1,8 @@
 import produce
 import cycle_connect
 import random
+import procedural_objects
+import numpy as np
 
 class Particle:
     def __init__(self, generic_object_list):
@@ -10,11 +12,14 @@ class Particle:
 
 
     def run_particle(self,intersection, start_type, steps, isFront):
+        cur_obj = start_obj(intersection, self.generic_object_list, start_type)
+        self.procedural_objects.append(cur_obj)
+
         if isFront:
-            self.procedural_objects += produce.execute_model(intersection, self.generic_object_list, start_type, steps)
+            self.procedural_objects += produce.execute_model(self.generic_object_list, cur_obj, steps)
             self.start_connect = self.procedural_objects[-1]
         else:
-            self.procedural_objects += produce.execute_model(intersection, self.generic_object_list, start_type, steps)
+            self.procedural_objects += produce.execute_model(self.generic_object_list, cur_obj, steps)
             self.end_connect = self.procedural_objects[-1]
 
     def run_connect(self):
@@ -31,9 +36,9 @@ class Particle:
         objStart = self.procedural_objects[rd1]
         objStart2 = self.procedural_objects[rd2]
 
-        self.procedural_objects += produce.execute_model2(self.generic_object_list, objStart, steps)
+        self.procedural_objects += produce.execute_model(self.generic_object_list, objStart, steps)
         self.start_connect = self.procedural_objects[-1]
-        self.procedural_objects += produce.execute_model2(self.generic_object_list, objStart2, steps)
+        self.procedural_objects += produce.execute_model(self.generic_object_list, objStart2, steps)
         self.end_connect = self.procedural_objects[-1]
 
     def overlapping_check(self):
@@ -68,3 +73,18 @@ class Particle:
                 break
 
         return (rd1, rd2)
+
+
+def start_obj(start_pos, generic_object_list, start_type):
+
+    cur_type = start_type
+    start_scope = generic_object_list[cur_type].scope
+    gen_hash = generic_object_list[cur_type].generate_hash()
+    cur_obj = procedural_objects.Procedural_object(cur_type, start_pos, start_scope, gen_hash)
+    cur_obj_x = cur_obj.length[0]
+    cur_obj_y = cur_obj.length[1]
+    cur_obj_z = cur_obj.length[2]
+    update_pos = np.array([cur_obj_x, cur_obj_y, cur_obj_z])
+    cur_obj.arbitrary_set_position(start_pos - update_pos)
+
+    return cur_obj
--- produce.py
+++ produce.py
@@ -2,51 +2,7 @@ import generic_objects
 import procedural_objects
 import numpy as np
 
-def execute_model(start_pos, generic_object_list, start_type, steps):
-    production_list = []
-    
-    cur_obj = start_obj(start_pos, generic_object_list, start_type)
-    production_list.append(cur_obj)
-
-    #processing
-    count = 0
-    not_end = True
-    while(count < steps or not_end):
-
-        tempt_count = count
-        next_type = None
-
-        while(tempt_count >= 0 and next_type == None):
-            cur_type = production_list[tempt_count].type
-            cur_generic_obj = generic_object_list[cur_type]
-            next_type = cur_generic_obj.get_nextType()
-            tempt_count -=1
-
-        next_generic_obj = generic_object_list[next_type]
-        next_scope = next_generic_obj.scope
-        next_hash = next_generic_obj.generate_hash()
-        next_obj = procedural_objects.Procedural_object(next_type, start_pos, next_scope, next_hash)
-        next_choice = cur_generic_obj.execute_rule(next_type)
-        cur_obj = production_list[tempt_count+1]
-        next_obj.set_position(cur_obj, next_choice)
-        cur_obj.add_connected(next_choice)
-        next_obj.add_connected(opposite_direction(next_choice))
-
-
-        production_list.append(next_obj)
-        cur_obj = next_obj
-
-        if next_generic_obj.canTerminate == "False":
-            not_end = True
-        else:
-            not_end = False
-    
-        count += 1
-
-    return production_list
-
-
-def execute_model2(generic_object_list, start_obj, steps):
+def execute_model(generic_object_list, start_obj, steps):
     production_list = []
     
     cur_obj = start_obj
@@ -210,20 +166,6 @@ def adjust_scope(production_list):
             break
     return production_list
        
-
-def start_obj(start_pos, generic_object_list, start_type):
-
-    cur_type = start_type
-    start_scope = generic_object_list[cur_type].scope
-    gen_hash = generic_object_list[cur_type].generate_hash()
-    cur_obj = procedural_objects.Procedural_object(cur_type, start_pos, start_scope, gen_hash)
-    cur_obj_x = cur_obj.length[0]
-    cur_obj_y = cur_obj.length[1]
-    cur_obj_z = cur_obj.length[2]
-    update_pos = np.array([cur_obj_x, cur_obj_y, cur_obj_z])
-    cur_obj.arbitrary_set_position(start_pos - update_pos)
-
-    return cur_obj
 
 def direction_to_index(direction):
     if direction == '+x' or direction == '-x':
