8f3fbee61808d75985b31df579f8bc438d071a72
Liyb2002 <yl4092@columbia.edu>
2023-05-15 21:05:04 -0400

    reading rooftop

--- assign_type.py
+++ assign_type.py
@@ -1,4 +1,6 @@
 import copy
+import random
+import numpy as np
 
 def assign(procedural_objects):
     for obj in procedural_objects:
@@ -10,6 +12,9 @@ def assign(procedural_objects):
                     assign_available = False
             
             if assign_available:
+                offset = random.uniform(0.3, 0.6)
+                obj.position += np.array([0, offset, 0])
+                obj.length += np.array([0, offset, 0])
                 obj.type = 9
     
     return procedural_objects
