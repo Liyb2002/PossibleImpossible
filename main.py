336ec11ef9e4be267b7085c64d512d9c6de64f83
Liyb2002 <yl4092@columbia.edu>
2023-06-04 22:16:36 +0800

    need to think more about particles

--- main.py
+++ main.py
@@ -45,41 +45,53 @@ steps = 2
 
 particle_list = []
 score_list = []
-num_particles = 100
+num_particles = 300
 
 for i in range(num_particles):
     tempt_particle = particle.Particle(generic_object_list)
     tempt_score = tempt_particle.get_score()
-    particle_list.append(tempt_particle)
-    score_list.append(tempt_score)
 
+    ok1 = tempt_particle.run_particle(foreground_intersection, foreground_type, foreground_connect, steps, foreground_parsedProb, True)
+    ok2 = tempt_particle.run_particle(background_intersection, background_type, background_connect, steps, background_parsedProb, False)
+
+    if ok1 and ok2:
+        particle_list.append(tempt_particle)
+        score_list.append(tempt_score)
 
 particle_list = resample.resample_particles(particle_list, score_list)
 
+score_list = []
+result_particle = None
+for i in range(len(particle_list)):
+    particle_list[i].run_connect()
+    if particle_list[i].success:
+        print("success")
+        result_particle = particle_list[i]
+        break
 
 
 
-success = False
-while(success != True):
-    print("-----------------")
-    cur_particle = particle.Particle(generic_object_list)
-    ok = cur_particle.run_particle(foreground_intersection, foreground_type, foreground_connect, steps, foreground_parsedProb, True)
-    if not ok :
-        continue
-    ok = cur_particle.run_particle(background_intersection, background_type, background_connect, steps, background_parsedProb, False)
-    if not ok :
-        continue
+# success = False
+# while(success != True):
+#     print("-----------------")
+#     cur_particle = particle.Particle(generic_object_list)
+#     ok = cur_particle.run_particle(foreground_intersection, foreground_type, foreground_connect, steps, foreground_parsedProb, True)
+#     if not ok :
+#         continue
+#     ok = cur_particle.run_particle(background_intersection, background_type, background_connect, steps, background_parsedProb, False)
+#     if not ok :
+#         continue
 
-    cur_particle.run_connect()
-    # cur_particle.run_particle2(steps+2)
+#     cur_particle.run_connect()
+#     # cur_particle.run_particle2(steps+2)
 
-    cur_particle.overlapping_check()
-    success = cur_particle.success
+#     cur_particle.overlapping_check()
+#     success = cur_particle.success
 
-procedural_objects = assign_type.assign(cur_particle.procedural_objects)
+procedural_objects = assign_type.assign(result_particle.procedural_objects)
 
 decorator = decorations.decoration_operator()
-decoration_list = decorator.decorate(cur_particle.procedural_objects)
+decoration_list = decorator.decorate(result_particle.procedural_objects)
 
 print("success!")
 output_writer = write2JSON.output()
