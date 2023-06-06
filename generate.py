e6856d95621c2fa3fd69b8f3a2a710b0dd9e19ff
Liyb2002 <yl4092@columbia.edu>
2023-06-06 16:21:56 +0800

    connect 2

--- generate.py
+++ generate.py
@@ -19,69 +19,61 @@ class generate_helper:
         foreground_intersection = basic_scene.get_possible_intersects(foreground_index)
         background_intersection = basic_scene.get_possible_intersects(background_index)
 
-        #parse probability tree
-        #start produce
+        self.generic_object_list = generic_object_list
+
+        self.particle_list = []
+        self.score_list = []
+
+        num_particles = 1000
+        for i in range(num_particles):
+            tempt_particle = particle.Particle(self.generic_object_list)
+            self.particle_list.append(tempt_particle)
+
         foreground_type = 1
         foreground_connect = "-y"
-        foreground_parsedProb = parseTree.parseProb(generic_object_list, generic_object_list[foreground_type])
-
         background_type = 3
         background_connect = "+y"
-        background_parsedProb = parseTree.parseProb(generic_object_list, generic_object_list[background_type])
-
         steps = 3
 
-        particle_list = []
+        self.generate_part(foreground_type, foreground_connect, foreground_intersection, steps, True)
+        self.generate_part(background_type, background_connect, background_intersection, steps, False)
+        self.connect()
+        
+    
+    def generate_part(self, start_type, connect_direction, intersection_pos, steps, isFront):
+        parsedProb = parseTree.parseProb(self.generic_object_list, self.generic_object_list[start_type])
+
         score_list = []
-        num_particles = steps * 100
 
-
-        for i in range(num_particles):
-            tempt_particle = particle.Particle(generic_object_list)
-            tempt_particle.prepare_particle(foreground_intersection, foreground_type, foreground_connect, foreground_parsedProb)
-            particle_list.append(tempt_particle)
+        for i in range(len(self.particle_list)):
+            tempt_particle = self.particle_list[i]
+            tempt_particle.prepare_particle(intersection_pos, start_type, connect_direction, parsedProb)
 
         for s in range(steps):
             cur_step = steps - s -1
             print("cur_step", cur_step)
 
             score_list = []
-            for i in range(len(particle_list)):
-                tempt_particle = particle_list[i]
-                tempt_particle.run_step(cur_step, True)
+            for i in range(len(self.particle_list)):
+                tempt_particle = self.particle_list[i]
+                tempt_particle.run_step(cur_step, isFront)
                 score_list.append(tempt_particle.get_score())
 
-            particle_list = resample.resample_particles(particle_list, score_list)
+            self.particle_list = resample.resample_particles(self.particle_list, score_list)
 
-        print("front generation complete")
-
-        for i in range(len(particle_list)):
-            tempt_particle = particle_list[i]
-            tempt_particle.prepare_particle(background_intersection, background_type, background_connect, background_parsedProb)
-            
-        for s in range(steps):
-            cur_step = steps - s -1
-
-            score_list = []
-            for i in range(len(particle_list)):
-                tempt_particle = particle_list[i]
-                tempt_particle.run_step(cur_step, False)
-                score_list.append(tempt_particle.get_score())
-
-            particle_list = resample.resample_particles(particle_list, score_list)
-
-        print("back generation complete")
+        print("generation complete")
 
 
+
+    def connect(self):
         result_particle = None
-        print("len(particle_list)", len(particle_list))
-        for i in range(len(particle_list)):
-            particle_list[i].run_connect()
-            if particle_list[i].success:
+        print("len(particle_list)", len(self.particle_list))
+        for i in range(len(self.particle_list)):
+            self.particle_list[i].run_connect()
+            if self.particle_list[i].success:
                 print("success")
-                result_particle = particle_list[i]
+                result_particle = self.particle_list[i]
                 break
-
 
 
         procedural_objects = assign_type.assign(result_particle.procedural_objects)
