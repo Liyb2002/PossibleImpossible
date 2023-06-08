4d7ad7aa8f7eac6049f1126798a38c8969c1302e
Liyb2002 <yl4092@columbia.edu>
2023-06-08 21:02:06 +0800

    terminal and stairs

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ class_generate = generate.generate_helper(generic_object_list)
 decoration_list = class_generate.smc_process()
 
 
-phase1, phase2, phase3 = class_generate.recursive_process()
+# phase1, phase2, phase3 = class_generate.recursive_process()
 
 print("success!")
 
 
-for obj in phase3:
-    print("obj id", obj.type)
+
+
 output_writer = write2JSON.output()
 # output_writer.prepare_write_debug(cur_particle.procedural_objects)
 # output_writer.prepare_write_skeleton(phase1)
@@ -37,6 +37,6 @@ output_writer = write2JSON.output()
 # output_writer.prepare_write_skeleton(phase1)
 # output_writer.write_phase2()
 
-output_writer.prepare_write_decorations(phase3)
+output_writer.prepare_write_decorations(decoration_list)
 output_writer.write()
 
