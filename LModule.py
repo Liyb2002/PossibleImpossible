8e6e74d2c27641701f7f96119c0decf9ecd1c0ea
Liyb2002 <yl4092@columbia.edu>
2023-07-04 19:04:18 +0800

    rule with some rotation

--- LModule.py
+++ LModule.py
@@ -2,21 +2,50 @@ import procedural_objects
 import numpy as np
 
 class Module:
-    def __init__(self, position, size):
-        self.type = 'A'
+    def __init__(self, position, size, rotation):
+        self.type = 11
         self.position = position
         self.size = size
         self.age = 0
+        self.rotation = rotation
 
     def toProcedual(self):
         dummy_scope = [0.1, 0.1]
 
-        tempt_obj = procedural_objects.Procedural_object(11, self.position,np.array([dummy_scope,dummy_scope,dummy_scope]), "00000", np.array([[0],[0],[0]]), np.array([0,0,0]))
+        tempt_obj = procedural_objects.Procedural_object(11, self.position,np.array([dummy_scope,dummy_scope,dummy_scope]), "00000", np.array([[self.rotation[0]],[self.rotation[1]],[self.rotation[2]]]), np.array([0,0,0]))
         tempt_obj.arbitrary_set_length(np.array([float(self.size[0]),float(self.size[1]),float(self.size[2])]))
         return tempt_obj
     
-    def execute(self, enviroment):
-        new_size = self.size
-        new_position = self.position + np.array([self.size[0], 0, 0]) + np.array([new_size[0], 0, 0])
-        new_module = Module(new_position, new_size)
-        return new_module
+    def execute(self, enviroment, rules):
+        #chose a rule to execute
+        execute_rule =  None
+        new_modules = []
+        
+        for rule in rules:
+            print("rule.lhs_type", rule.lhs_type, "self.type", self.type)
+            if rule.lhs_type == self.type:
+                execute_rule = rule
+                break
+        
+        for i in range(len(execute_rule.rhs_types)):
+            new_type = execute_rule.rhs_types[i]
+            new_size = np.array([self.size[0] * execute_rule.rhs_size_multiplier[i][0], self.size[1] * execute_rule.rhs_size_multiplier[i][1], self.size[2] * execute_rule.rhs_size_multiplier[i][2]])
+            new_rotation = self.rotation + execute_rule.rhs_rotations[i]
+            new_direction = execute_rule.rhs_directions[i]
+
+            new_position = self.get_new_position(new_direction, new_size)
+            new_module = Module(new_position, new_size, new_rotation)
+            new_modules.append(new_module)
+
+        return new_modules
+
+
+    def get_new_position(self, new_direction, new_size):
+        new_position = np.array([0,0,0])
+        if new_direction == "top":
+            new_position = self.position + np.array([0, self.size[1], 0]) + np.array([0, new_size[1], 0])
+        
+        if new_direction == "bot":
+            new_position = self.position + np.array([0, self.size[1], 0]) + np.array([0, new_size[1], 0])
+
+        return new_position
--- LSystem.py
+++ LSystem.py
@@ -12,14 +12,16 @@ class LSys:
         self.steps = 4
         self.procedural_objects = []
         self.new_objects = []
+        self.rules = []
         self.light_pos = np.array([0.0,0.0,0.0])
         self.startpos = np.array([0.0,0.0,0.0])
 
+        self.add_rule()
         self.init_state()
         self.run_system()
 
     def init_state(self):
-        start_module = LModule.Module(self.startpos, np.array([0.1,0.05,0.05]))
+        start_module = LModule.Module(self.startpos, np.array([0.1,0.05,0.05]), np.array([0,0,0]))
         self.new_objects.append(start_module)
     
     def run_system(self):
@@ -34,8 +36,22 @@ class LSys:
             if count > level_count:
                 break
             count += 1
-            self.new_objects.append(obj.execute(self.light_pos))
+            self.new_objects += obj.execute(self.light_pos, self.rules)
             self.procedural_objects.append(obj.toProcedual())
     
+    def add_rule(self):
+        new_rule = rule(11)
+        self.rules.append(new_rule)
+
     def finish_system(self):
         return self.procedural_objects
+
+
+class rule:
+    def __init__(self, left_hand_type):
+        self.lhs_type = left_hand_type
+        self.rhs_types = [11, 11]
+        self.rhs_size_multiplier = [[0.8,0.8,0.8], [0.8,0.8,0.8]]
+        self.rhs_rotations = [[0.0, 0.0, 0.2], [0.0, 0.0, -0.2]]
+        self.rhs_offsets = [[0.0, 0.1, 0.0], [0.0, -0.1, 0.0]]
+        self.rhs_directions = ["top", "bot"]
