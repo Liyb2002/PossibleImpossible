be9994f436d9571f5e41663a2a406e17ea6e3d95
Liyb2002 <yl4092@columbia.edu>
2023-07-05 16:47:27 +0800

    a tree

--- LModule.py
+++ LModule.py
@@ -1,5 +1,6 @@
 import procedural_objects
 import numpy as np
+import random
 
 class Module:
     def __init__(self, position, size, rotation, age):
@@ -30,6 +31,7 @@ class Module:
             return []
             
         for i in range(len(execute_rule.rhs_types)):
+
             new_type = execute_rule.rhs_types[i]
             new_size = np.array([self.size[0] * execute_rule.rhs_size_multiplier[i][0], self.size[1] * execute_rule.rhs_size_multiplier[i][1], self.size[2] * execute_rule.rhs_size_multiplier[i][2]])
             new_rotation = self.rotation + execute_rule.rhs_rotations[i]
@@ -80,6 +82,28 @@ class Module:
 
                 new_position += (0 - np.array([0, self.size[1], 0]) - np.array([0, new_size[1], 0]) + prev_obj_delta_rotate + new_obj_delta_rotate)  * new_direction[1]
 
+            if new_direction[0] == "+z":
+                prev_obj_closest_point = self.position + np.array([0, 0, self.size[2]])
+                prev_obj_rotated_point = procedural_objects.rotate_line(self.position, prev_obj_closest_point, self.rotation[0],self.rotation[1],self.rotation[2])
+                prev_obj_delta_rotate = prev_obj_rotated_point - prev_obj_closest_point
+
+                new_obj_closest_point = np.array([0, 0, new_size[2]])
+                new_obj_rotated_point = procedural_objects.rotate_line(np.array([0,0,0]), new_obj_closest_point, self.rotation[0],self.rotation[1],self.rotation[2])
+                new_obj_delta_rotate = new_obj_rotated_point - new_obj_closest_point
+
+                new_position +=  (np.array([0, 0, self.size[2]]) + np.array([0, 0, new_size[2]]) + prev_obj_delta_rotate + new_obj_delta_rotate)  * new_direction[1]
+
+            if new_direction[0] == "-z":
+                prev_obj_closest_point = self.position - np.array([0, 0, self.size[2]])
+                prev_obj_rotated_point = procedural_objects.rotate_line(self.position, prev_obj_closest_point, self.rotation[0],self.rotation[1],self.rotation[2])
+                prev_obj_delta_rotate = prev_obj_rotated_point - prev_obj_closest_point
+
+                new_obj_closest_point = np.array([0, 0, -new_size[2]])
+                new_obj_rotated_point = procedural_objects.rotate_line(np.array([0,0,0]), new_obj_closest_point, self.rotation[0],self.rotation[1],self.rotation[2])
+                new_obj_delta_rotate = new_obj_rotated_point - new_obj_closest_point
+
+                new_position += (0 - np.array([0, 0, self.size[2]]) - np.array([0, 0, new_size[2]]) + prev_obj_delta_rotate + new_obj_delta_rotate)  * new_direction[1]
+
         return new_position
     
 
@@ -88,5 +112,12 @@ class Module:
             if condition[0] == 'age':
                 if condition[1] == 'less_than' and self.age >= condition[2]:
                     return False
-                        
+                
+                if condition[1] == 'larger_than' and self.age <=
+                 condition[2]:
+                    return False
+            
+            if condition[0] == 'Prob' and random.random() > condition[1]:
+                return False
+
         return True
